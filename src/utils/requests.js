f0dc94fa808093d6f656f5b7321d7101ed67d119
Nelson Mbau <mbau.ndungu@gmail.com>
2020-05-14 19:30:52 +0300

    feat: Proxy API requests to right server

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -4,7 +4,7 @@ if (window.location.href.includes('localhost')) {
   API = `http://localhost:4000`
   // API = `https://development-smartkids.herokuapp.com`
 } else {
-  API = `http://138.197.108.96:4000`
+  API = ''
 }
 
 export {API}

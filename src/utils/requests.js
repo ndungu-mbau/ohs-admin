c9e8cb1540e9bca25fe1d89bcd8b61fa4af58c6d
Nelson Mbau <mbau.ndungu@gmail.com>
2020-05-21 01:58:20 +0300

    fix: Use explicit IP

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -4,7 +4,7 @@ if (window.location.href.includes('localhost')) {
   API = `http://localhost:4000`
   // API = `https://development-smartkids.herokuapp.com`
 } else {
-  API = ''
+  API = 'http://158.101.165.50'
 }
 
 export {API}

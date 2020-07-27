c37f8debc5276441c4a9db45ad52fcf4d0a88017
Nelson Mbau <mbau.ndungu@gmail.com>
2020-07-27 07:28:42 +0300

    feat: Point to ohs graph from dev

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -2,9 +2,9 @@ let API = "";
 
 if (window.location.href.includes('localhost')) {
   API = `http://localhost:4000`
-  // API = `https://development-smartkids.herokuapp.com`
+  // API = `http://ohs.adriankenya.com`
 } else {
-  API = 'http://158.101.165.50'
+  API = 'http://ohs.adriankenya.com'
 }
 
 export {API}
